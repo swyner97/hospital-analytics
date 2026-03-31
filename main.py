553183430d45f503abc3f3a5c00c54627da351da
swyner97 <swyner97@gmail.com>
2026-03-31 12:25:45 -0700

    implement additional aggregation functions and bridge tables for safety, patient experience, and timely care footnotes

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ from src.data_cleaning import clean_footnotes, clean_readmissions, clean_general
 from src.transformations import (
     aggregate_readmissions,
     aggregate_mortality,
-    build_bridge_mort_footnote,
-    attach_footnote_desc
+        aggregate_overall_rating,
+        aggregate_safety,
+        aggregate_te,
+        aggregate_patient_experience,
+        build_bridge_mort_footnote,
+        build_bridge_ptexp_footnote,
+        build_bridge_readm_footnote,
+        build_bridge_safety_footnote,
+        build_bridge_te_footnote,
+        attach_footnote_desc
 )
 
 
@@ -23,9 +31,25 @@ def main():
     aggregated_readmissions = aggregate_readmissions(readmissions_clean)
     aggregated_mortality = aggregate_mortality(general_info_clean)
     bridge_mort_footnote = build_bridge_mort_footnote(general_info_clean)
+    bridge_readm_footnote = build_bridge_readm_footnote(general_info_clean)
+    bridge_ptexp_footnote = build_bridge_ptexp_footnote(general_info_clean)
+    bridge_safety_footnote = build_bridge_safety_footnote(general_info_clean)
+    bridge_te_footnote = build_bridge_te_footnote(general_info_clean)
 
     # preview join for checking
-    bridge_with_desc = bridge_mort_footnote.merge(
+    bridge_with_desc_m = bridge_mort_footnote.merge(
+        footnote_dim, on="footnote_code", how="left"
+    )
+    bridge_with_desc_s = bridge_safety_footnote.merge(
+        footnote_dim, on="footnote_code", how="left"
+    )
+    bridge_with_desc_r = bridge_readm_footnote.merge(
+        footnote_dim, on="footnote_code", how="left"
+    )
+    bridge_with_desc_p = bridge_ptexp_footnote.merge(
+        footnote_dim, on="footnote_code", how="left"
+    )
+    bridge_with_desc_t = bridge_te_footnote.merge(
         footnote_dim, on="footnote_code", how="left"
     )
 
@@ -42,11 +66,18 @@ def main():
     print("\n=== FOOTNOTE DIMENSION ===")
     print(footnote_dim.head())
 
-    print("\n=== BRIDGE TABLE ===")
-    print(bridge_mort_footnote.head())
-
     print("\n=== BRIDGE WITH DESCRIPTIONS ===")
-    print(bridge_with_desc.dropna(subset=["footnote_code"]).head())
+    print("=== MORTALITY FOOTNOTES ===")
+    print(bridge_with_desc_m.dropna(subset=["footnote_code"]).head())
+    print("=== SAFETY FOOTNOTES ===")
+    print(bridge_with_desc_s.dropna(subset=["footnote_code"]).head())
+    print("=== READMISSION FOOTNOTES ===")
+    print(bridge_with_desc_r.dropna(subset=["footnote_code"]).head())
+    print("=== PATIENT EXPERIENCE FOOTNOTES ===")
+    print(bridge_with_desc_p.dropna(subset=["footnote_code"]).head())
+    print("=== TIMELY AND EFFECTIVE CARE FOOTNOTES ===")
+    print(bridge_with_desc_t.dropna(subset=["footnote_code"]).head())
+
 
 
 if __name__ == "__main__":
--- src/transformations.py
+++ src/transformations.py
@@ -34,6 +34,68 @@ def aggregate_readmissions(df: pd.DataFrame) -> pd.DataFrame:
 
     return aggregated
 
+def aggregate_overall_rating(df: pd.DataFrame) -> pd.DataFrame:
+    aggregated = (
+        df.groupby("Facility ID")
+        .agg(
+            {
+                "Facility Name": "first",
+                "Hospital overall rating": "max",
+            }
+        )
+        .reset_index()
+    )
+
+    return aggregated
+
+def aggregate_safety(df: pd.DataFrame) -> pd.DataFrame:
+    aggregated = (
+        df.groupby("Facility ID")
+        .agg(
+            {
+                "Facility Name": "first",
+                "Safety Group Measure Count": "max",
+                "Count of Facility Safety Measures": "sum",
+                "Count of Safety Measures Better": "sum",
+                "Count of Safety Measures No Different": "sum",
+                "Count of Safety Measures Worse": "sum",
+            }
+        )
+        .reset_index()
+    )
+
+    return aggregated
+
+def aggregate_patient_experience(df: pd.DataFrame) -> pd.DataFrame:
+    aggregated = (
+        df.groupby("Facility ID")
+        .agg(
+            {
+                "Facility Name": "first",
+                "Pt Exp Group Measure Count": "max",
+                "Count of Facility Pt Exp Measures": "sum",
+            }
+        )
+        .reset_index()
+    )
+
+    return aggregated
+
+def aggregate_te(df: pd.DataFrame) -> pd.DataFrame:
+    aggregated = (
+        df.groupby("Facility ID")
+        .agg(
+            {
+                "Facility Name": "first",
+                "TE Group Measure Count": "max",
+                "Count of Facility TE Measures": "sum",
+            }
+        )
+        .reset_index()
+    )
+
+    return aggregated
+
 
 # This function creates a bridge table to link mortality measures with their corresponding footnotes
 def build_bridge_mort_footnote(df: pd.DataFrame) -> pd.DataFrame:
@@ -53,6 +115,73 @@ def build_bridge_mort_footnote(df: pd.DataFrame) -> pd.DataFrame:
     )
     return bridge
 
+def build_bridge_safety_footnote(df: pd.DataFrame) -> pd.DataFrame:
+    bridge = (
+        df[["Facility ID", "Safety Group Footnote"]]
+        .copy()
+        .astype("string")
+        .rename(
+            columns={
+                "Facility ID": "facility_id",
+                "Safety Group Footnote": "footnote_code",
+            }
+        )
+    )
+    bridge["footnote_code"] = (
+        bridge["footnote_code"].str.replace(r"\.0$", "", regex=True).str.strip()
+    )
+    return bridge
+
+def build_bridge_readm_footnote(df: pd.DataFrame) -> pd.DataFrame:
+    bridge = (
+        df[["Facility ID", "READM Group Footnote"]]
+        .copy()
+        .astype("string")
+        .rename(
+            columns={
+                "Facility ID": "facility_id",
+                "READM Group Footnote": "footnote_code",
+            }
+        )
+    )
+    bridge["footnote_code"] = (
+        bridge["footnote_code"].str.replace(r"\.0$", "", regex=True).str.strip()
+    )
+    return bridge
+
+def build_bridge_ptexp_footnote(df: pd.DataFrame) -> pd.DataFrame:
+    bridge = (
+        df[["Facility ID", "Pt Exp Group Footnote"]]
+        .copy()
+        .astype("string")
+        .rename(
+            columns={
+                "Facility ID": "facility_id",
+                "Pt Exp Group Footnote": "footnote_code",
+            }
+        )
+    )
+    bridge["footnote_code"] = (
+        bridge["footnote_code"].str.replace(r"\.0$", "", regex=True).str.strip()
+    )
+    return bridge
+
+def build_bridge_te_footnote(df: pd.DataFrame) -> pd.DataFrame:
+    bridge = (
+        df[["Facility ID", "TE Group Footnote"]]
+        .copy()
+        .astype("string")
+        .rename(
+            columns={
+                "Facility ID": "facility_id",
+                "TE Group Footnote": "footnote_code",
+            }
+        )
+    )
+    bridge["footnote_code"] = (
+        bridge["footnote_code"].str.replace(r"\.0$", "", regex=True).str.strip()
+    )
+    return bridge
 
 def attach_footnote_desc(
     bridge: pd.DataFrame, dim_footnote: pd.DataFrame
